b2c83185358088d8f8e21db63fa808f66eb67044
Chris Judkins <mail@chrisjudkins.com>
2017-10-01 21:26:45 -0400

    Made AddNode have type-independent inputs

--- Assets/Plugins/Node_Editor/Nodes/Math/AddNode.cs
+++ Assets/Plugins/Node_Editor/Nodes/Math/AddNode.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using NodeEditorFramework;
 using NodeEditorFramework.Utilities;
 
@@ -12,7 +13,6 @@ namespace NodeEditorFramework.Standard
 
 		public float summand1 = 0f;
 		public float summand2 = 0f;
-		protected string label = "";
 		
 		public override Node Create (Vector2 pos) 
 		{
@@ -21,17 +21,19 @@ namespace NodeEditorFramework.Standard
 			node.rect = new Rect (pos.x, pos.y, 150, 70);
 			node.name = "Add";
 			
-			node.CreateInput ("Summand 1", "Float");
-			node.CreateInput ("Summand 2", "Float");
-			node.CreateOutput ("Sum", "Float");
+			NodeInput.Create(node, "Summand 1", typeof(object).AssemblyQualifiedName);
+			NodeInput.Create(node, "Summand 2", typeof(object).AssemblyQualifiedName);
+
+			// node.CreateInput ("Summand 1", "Float");
+			// node.CreateInput ("Summand 2", "Float");
+
+			node.CreateOutput("Sum", "Float");
 			
 			return node;
 		}
 		
 		protected internal override void NodeGUI () 
 		{
-			//	GUILayout.Label (label);
-
 			GUILayout.Space(5f);
 
 			GUILayout.BeginHorizontal ();
@@ -62,22 +64,34 @@ namespace NodeEditorFramework.Standard
 			
 			GUILayout.EndVertical ();
 			GUILayout.EndHorizontal ();
+
+			if (GUI.changed) {
+				NodeEditor.RecalculateFrom(this);
+			}
 			
 		}
 		
 		public override bool Calculate () 
 		{
+			object input1 = null;
+			object input2 = null;
+
 			if (!allInputsReady ())
 				return false;
 
-			if (Inputs[0].connection != null)
-				summand1 = Inputs[0].connection.GetValue<float> ();
-			if (Inputs[1].connection != null)
-				summand2 = Inputs[1].connection.GetValue<float> ();
+			if (Inputs[0].connection != null) {
+				input1 = Inputs[0].connection.GetValue();
+			}
 
-			Outputs[0].SetValue<float> (summand1 + summand2);
+			if (Inputs[1].connection != null) {
+				input2 = Inputs[1].connection.GetValue();
+			}
 
-			label = Outputs[0].GetValue(typeof(float)).ToString();
+			input1 = (float)Convert.ChangeType(input1, typeof(float));
+			input2 = (float)Convert.ChangeType(input2, typeof(float));
+
+			float result = (float)input1 + (float)input2;
+			Outputs[0].SetValue (result);
 
 			return true;
 		}
